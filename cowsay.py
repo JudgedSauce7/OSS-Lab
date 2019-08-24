d51382e10161a3779456b0f3e560e89420b67f07
JudgedSauce7 <32275143+JudgedSauce7@users.noreply.github.com>
2019-08-24 15:34:27 +0530

    Just a fun little hello.

---
+++ cowsay.py
@@ -0,0 +1,12 @@
+def cowsay(str):
+	print("""
+	< %s >
+	 ---
+	        \   ^__^
+	         \  (oo)\_______
+	            (__)\       )\/
+	                ||----w |
+	                ||     ||""" %(str))
+	return;
+
+cowsay("Hello Git")
